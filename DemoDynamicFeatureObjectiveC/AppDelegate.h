7ecbcd420d9a9ee4719ef312e7c04b0024bc57de
Phuong <phuongnhu0894@gmail.com>
2015-11-10 17:03:54 +0700

    First Commit

---
+++ DemoDynamicFeatureObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoDynamicFeatureObjectiveC
+//
+//  Created by Cuong Trinh on 9/30/15.
+//  Copyright © 2015 Cuong Trinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoDynamicFeatureObjectiveC/ViewController.m
@@ -0,0 +1,105 @@
+#import "ViewController.h"
+
+#pragma mark - Animals
+@interface Cat : NSObject
+@end
+
+@implementation Cat
+- (void) say {
+    NSLog(@"Meo meo");
+}
+@end
+
+@interface Dog : NSObject
+@end
+@implementation Dog
+- (void) say {
+    NSLog(@"Gau Gau");
+}
+@end
+
+@interface Mouse : NSObject
+@end
+@implementation Mouse
+- (void) say {
+    NSLog(@"Chit chit");
+}
+@end
+
+#pragma mark - Protocols
+@protocol Fly <NSObject>
+-(void) fly;
+@end
+
+@protocol Swim <NSObject>
+-(void) swim;
+@end
+
+@interface Turtle : NSObject <Swim>
+@end
+@implementation Turtle
+-(void) swim {
+    NSLog(@"I can swim");
+}
+@end
+#pragma mark - Main Logic
+@implementation ViewController
+
+
+- (IBAction)createObjectFromDynamicClass:(id)sender {
+    NSArray* animals = @[@"Cat", @"Dog", @"Mouse", @"Elephant", @"Tiger"];
+    int index = arc4random_uniform((u_int32_t)animals.count);
+    
+    Class class = NSClassFromString(animals[index]);
+    if (class == nil) {
+        NSLog(@"Non exist class - %@", animals[index]);
+        return;
+    }
+    id object = [class new];
+    SEL sayMethod = @selector(say);
+    if ([object respondsToSelector:sayMethod]) {
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [object performSelector:sayMethod];
+        #pragma clang diagnostic pop
+    }
+    
+}
+
+- (IBAction)callMethodFromString:(id)sender {
+    NSArray* methods = @[@"sayHello", @"sayName", @"rob bank"];
+    int index = arc4random_uniform((u_int32_t)methods.count);
+    
+    SEL method = NSSelectorFromString(methods[index]);
+    
+    if ([self respondsToSelector:method]) {
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [self performSelector:method];
+        #pragma clang diagnostic pop
+    } else {
+        NSLog(@"%@ is not method",methods[index]);
+    }
+
+}
+
+- (IBAction)createProtocolFromString:(id)sender {
+    NSArray* protocols = @[@"Fly", @"Swim"];
+    Turtle* turtle = [Turtle new];
+    for (NSString* protocolName in protocols) {
+        Protocol* protocol = NSProtocolFromString(protocolName);
+        if ([turtle conformsToProtocol:protocol]) {
+            NSLog(@"Turtle adopts %@ protocol", protocolName);
+        }
+    }
+}
+
+#pragma mark - Some dummy methods
+-(void) sayHello {
+    NSLog(@"Hello");
+}
+
+-(void) sayName{
+    NSLog(@"I am Cuong");
+}
+@end
